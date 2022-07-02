cd71c023237ec12043b2310e9e02dd5589238fe0
CheikhMohamedD <cheikhdiack313@outlook.fr>
2022-07-02 23:40:20 +0000

    vscode config

---
+++ Expanding Card/script.js
@@ -0,0 +1,15 @@
+// to select the card
+const panels = document.querySelectorAll(".panel");
+//add .active on the card when clicked
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active")
+  });
+}
