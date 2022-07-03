9cd36c8e733f60e5a5bb9c5dd13c0ff96a5f35d7
CheikhMohamedD <cheikhdiack313@outlook.fr>
2022-07-03 13:06:47 +0000

    progress Step finished

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -2,7 +2,7 @@ const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
-// declenchez l'animation au Click sur le boutton Next ou preview
+// declenchez l'animation de la fonction update au Click sur le boutton Next ou preview
 let currentActive = 1; // represente le stamp qui ne depasse pas le nombre de classe .circle (dans notre cas 4 mais s'adapte en augmentant)
 next.addEventListener("click", () => {
   currentActive++;
@@ -19,8 +19,18 @@ prev.addEventListener("click", () => {
   update();
 });
 
-
-
+/** la fonction Update s'occupe de l'animation de la progress Step en general.
+ *
+ * Elle a pour role d'ajouter ou de supprimer les classes .active
+ * sur les div.circle .
+ * Elle permet egalement de recuperer les classes .active
+ * ajoutee par la boucle if par rapport a la valeur currentActive
+ * et de modifier le style du div#progress en divisant
+ * ((la longueur de .active-1) / (la longueur de circle-1 )
+ * et en multipliant le tout par 100% .
+ *
+ * Anime aussi les buttons suivant la comparaison currentActive et le nombre de cercle actifs circle.length
+ */
 function update() {
   circles.forEach((circle, index) => {
     if (index < currentActive) {
@@ -30,5 +40,16 @@ function update() {
     }
   });
   const actives = document.querySelectorAll(".active");
-  progress.style.width = (actives.length-1) / (circles.length-1) * 100 + "%";
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  //Pour l'animation logique des bouttons Next et previous
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
